6b5a218ecf232c74a65ac2faafe088b529dc1d50
bradleyyam <bradley.yam@gmail.com>
2021-03-03 02:41:51 -0500

    fixed db issue

--- server/data/urlDb.js
+++ server/data/urlDb.js
@@ -1,13 +1,12 @@
 const Url = require("../models/orm");
 
 const save = (longURL, shortURL, shortUrlId) => {
-    Url.sync({ force: true }).then(() => {
-        // Now the `urls` table in the database corresponds to the model definition
+    // Url.sync({ force: true }).then(() => {
+        // Now the `urls` table in the database corresponds to the model definition, otherwise must sync
         return Url.create({shortUrlId: shortUrlId, longURL: longURL, shortURL: shortURL}).then(url => {
             console.log("Saving longURL:", url.longURL);
         });
-      });
-    
+    //   });
 }
 
 const find = (shortUrlId) => {
